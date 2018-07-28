126e128d795bd2f204005bf47081031e86f98478
Ierofantis <evil69669@gmail.com>
2018-07-28 15:25:39 +0300

    Frontend Routes and Notes

---
+++ MonsterFront/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'profile', component: FrontpageComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'registration', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    FrontpageComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  exports: [ RouterModule ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    [ RouterModule.forRoot(routes) ]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
