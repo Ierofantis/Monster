608c2b7b47173ca599982b171e0b17b6084e699a
Ierofantis <evil69669@gmail.com>
2018-08-05 22:55:58 +0300

    Test

--- MonsterFront/src/app/frontpage/frontpage.component.ts
+++ MonsterFront/src/app/frontpage/frontpage.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frontpage.component.css']
 })
 export class FrontpageComponent implements OnInit {
+  data: Object[];
 
   constructor() { }
 
   ngOnInit() {
+    this.data = [
+      {
+        testHeader:"Test One",
+        test:"one"
+      },
+      {
+        testHeader:"Test Two",
+        test:"two"
+      },
+      {
+        testHeader:"Test Three",
+        test:"three"
+      },
+    ];
   }
+  
 
 }
